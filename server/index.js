03e46bb2ce7c55b1fb8ae3462daa1de0ff127831
ramareddy12345 <ramareddy114@gmail.com>
2021-11-19 16:53:05 +0530

    changed port

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 
 app.use(instance);
-
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log('Server started in PORT:', 3000);
 });
